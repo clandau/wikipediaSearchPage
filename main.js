fc85df2966df2be7eb56d59e355d22301b413dd7
clandau <so.super@gmail.com>
2017-09-15 12:48:24 -0400

    initial commit of full project

---
+++ main.js
@@ -0,0 +1,78 @@
+var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="
+var urlEnd = "&srprop=snippet&origin=*";
+var searchEntered = document.getElementById("searchbox");
+var resultsExist = false;  //identifies whether results are already on page for button handler
+var searchboxVisible = true;
+var clearButtonVisible = false;
+var clearButton = document.getElementById('clrBtn');
+var resultsList = document.getElementById('output');
+var searchbox = document.getElementById('searchbox-div');
+
+searchEntered.addEventListener("keyup", function(e){
+    if (e.which === 13) {  //checks whether key was enter key
+        resultsExist = true;
+        //showOrHideElements(searchEntered);
+        searchboxVisible = false;
+        showOrHideElements(searchbox);
+        var completeUrl = url + searchEntered.value + urlEnd;
+        var request = new XMLHttpRequest();
+        request.open("GET", completeUrl, true);
+        request.onload = function() {
+            if (request.status >= 200 && request.status<400) {
+                var returnData = JSON.parse(request.responseText);
+                renderHTML(returnData);  //output results
+                //showOrHideElements(clearButton);
+                clearButton.style.display = null;
+                clearButtonVisible = true;
+            }
+            else {
+                console.log("we connected to the server but it returned an error");
+        }
+    }
+/*handling connection errors */
+request.onerror = function() {
+    console.log("connection error");
+}
+request.send();
+    }
+});
+
+//handle data from API and display search results
+function renderHTML(data) {
+    var resultsNum = data.query.search.length;
+    var url = "https://en.wikipedia.org/wiki/";
+    var getListElem = document.getElementById("output");
+    for (var i=0; i<resultsNum; i++) {
+        var newLi = document.createElement("li");
+        newLi.id = "item" + i;
+        var title = data.query.search[i].title;
+        newLi.innerHTML += "<h3><a href='" + url + title + "'target='_blank'>" + title + "</a></h3>";
+        newLi.innerHTML += '<p>' + data.query.search[i].snippet;
+        getListElem.appendChild(newLi);
+    }
+}
+
+function showOrHideElements(loc) {
+    if (loc.style.display === 'none') {
+        loc.style.display = 'block';
+        console.log("this was called " + loc);
+    }
+        loc.style.display = 'none';
+}
+
+clearButton.addEventListener('click', function() {
+        if (resultsExist) {
+            //take out list elems
+            resultsList.innerHTML = ' ';
+            resultsExist = false;
+            showOrHideElements(clearButton);     //hide clear button
+            searchbox.style.display = 'block';   //put the search box back on the page
+            searchboxVisible = true;
+            searchEntered.value = null;          //clear the contents of the search box
+        }
+});
+
+//reset results and return to clear search box screen
+// function clearSearch() {
+
+// }
